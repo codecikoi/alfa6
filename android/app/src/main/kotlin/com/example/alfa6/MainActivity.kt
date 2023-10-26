21349e4437d8669e9ccd4bab5d3e68de3b947a7a
wowdobryy <wowdobryy@gmail.com>
2023-10-26 06:28:23 +0300

    initial

---
+++ android/app/src/main/kotlin/com/example/alfa6/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alfa6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/dhikr_activity_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DhikrActivityPage extends StatelessWidget {
+  const DhikrActivityPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
